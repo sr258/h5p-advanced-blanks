ae6c224f97a2c0a6908401e816b702337b2adea8
sr258 <serettig@gmail.com>
2017-08-15 22:05:39 +0100

    Removed static content to transition to H5P.Question

--- src/scripts/controllers/advanced-blanks-controller.ts
+++ src/scripts/controllers/advanced-blanks-controller.ts
@@ -1,5 +1,4 @@
 ﻿import { Cloze } from "../models/cloze";
-import { Feedback } from "../models/feedback";
 import { IDataRepository } from "../services/data-repository";
 import { ISettings } from "../services/settings";
 import { H5PLocalization, LocalizationLabels } from "../services/localization";
@@ -12,26 +11,22 @@ import * as Ractive from 'ractive';
 
 export class AdvancedBlanksController {
   private cloze: Cloze;
-  private feedback: Feedback;
   private checkAllLabel: string;
   private isSelectCloze: boolean;
 
   // Storage of the ractive objects that link models and views
   private highlightRactives: { [id: string]: Ractive.Ractive } = {};
   private blankRactives: { [id: string]: Ractive.Ractive } = {};
-  private staticRactive: Ractive.Ractive;
 
   constructor(private repository: IDataRepository, private jquery: JQuery, private settings: ISettings, private localization: H5PLocalization) {
   }
-  
+
   /**
    * Sets up all blanks, the cloze itself and the ractive bindings.
    * @param  {HTMLElement} root
    */
   initialize(root: HTMLElement) {
     this.isSelectCloze = this.settings.clozeType == ClozeType.Select ? true : false;
-    this.checkAllLabel = this.localization.getTextFromLabel(LocalizationLabels.checkAllButton);
-    this.feedback = new Feedback(this.repository.getFeedbackText());
 
     var blanks = this.repository.getBlanks();
 
@@ -47,18 +42,52 @@ export class AdvancedBlanksController {
     this.createRactiveBindings();
   }
 
-  private createAndAddContainers(addTo: HTMLElement): { cloze: HTMLDivElement, static: HTMLDivElement } {
+  checkAll = () => {
+    this.hideAllHighlights();
+    for (var blank of this.cloze.blanks) {
+      if ((!blank.isCorrect) && blank.enteredText != "")
+        blank.evaluateEnteredAnswer();
+    }
+    this.refreshCloze();
+    this.checkAndNotifyCompleteness();
+  }
+
+  showHint = (blank: Blank) => {
+    this.hideAllHighlights();
+    blank.showHint();
+    this.refreshCloze();
+  }
+
+  requestCloseTooltip = (blank: Blank) => {
+    blank.removeTooltip();
+    this.refreshCloze();
+    this.jquery.find("#" + blank.id).focus();
+  }
+
+  checkBlank = (blank: Blank) => {
+    this.hideAllHighlights();
+    blank.evaluateEnteredAnswer();
+    this.refreshCloze();
+
+    if (!this.checkAndNotifyCompleteness()) {
+      if (blank.isCorrect) {
+        // move to next blank
+        var index = this.cloze.blanks.indexOf(blank);
+        if (index + 1 >= this.cloze.blanks.length)
+          return;
+        var nextId = this.cloze.blanks[index + 1].id;
+        this.jquery.find("#" + nextId).focus();
+      }
+    }
+  }
+
+  private createAndAddContainers(addTo: HTMLElement): { cloze: HTMLDivElement } {
     var clozeContainerElement = document.createElement('div');
     clozeContainerElement.id = 'clozeContainer';
     addTo.appendChild(clozeContainerElement);
 
-    var staticContainerElement = document.createElement('div');
-    staticContainerElement.id = 'staticContainer';
-    addTo.appendChild(staticContainerElement);
-
     return {
-      cloze: clozeContainerElement,
-      static: staticContainerElement
+      cloze: clozeContainerElement
     };
   }
 
@@ -85,25 +114,11 @@ export class AdvancedBlanksController {
         escape: RactiveEventsKeys.escape
       }
     });
-    ractive.on("checkCloze", this.onCheckBlank);
-    ractive.on("showHint", this.onShowHint);
-    ractive.on("closeMessage", this.onRequestCloseHighlight);
+    ractive.on("checkCloze", this.checkBlank);
+    ractive.on("showHint", this.showHint);
+    ractive.on("closeMessage", this.requestCloseTooltip);
 
     this.blankRactives[blank.id] = ractive;
-  }
-
-  private bindStatic() {
-    this.staticRactive = new Ractive({
-      el: '#staticContainer',
-      template: require('../views/static.ractive.html'),
-      data: {
-        feedback: this.feedback,
-        checkAllLabel: this.checkAllLabel,
-      }
-    });
-    this.staticRactive.on("checkAll", this.onCheckAll);
-    this.staticRactive.on("checkCloze", this.onCheckBlank);
-    this.staticRactive.on("closeFeedback", this.onRequestCloseFeedback);
   }
 
   private createRactiveBindings() {
@@ -114,8 +129,6 @@ export class AdvancedBlanksController {
     for (var blank of this.cloze.blanks) {
       this.createBlankBinding(blank);
     }
-
-    this.bindStatic();
   }
 
   /**
@@ -134,59 +147,13 @@ export class AdvancedBlanksController {
     }
   }
 
-  onRequestCloseFeedback = () => {
-    this.feedback.isVisible = false;
-    this.staticRactive.set("feedback", this.feedback);
-  }
-
   private checkAndNotifyCompleteness = (): boolean => {
     if (this.cloze.checkCompleteness()) {
       this.repository.setSolved();
-      this.feedback.isVisible = true;
-      this.staticRactive.set("feedback", this.feedback);
       return true;
     }
 
     return false;
-  }
-
-  onCheckAll = () => {
-    this.hideAllHighlights();
-    for (var blank of this.cloze.blanks) {
-      if ((!blank.isCorrect) && blank.enteredText != "")
-        blank.evaluateEnteredAnswer();
-    }
-    this.refreshCloze();
-    this.checkAndNotifyCompleteness();
-  }
-
-  onShowHint = (blank: Blank) => {
-    this.hideAllHighlights();
-    blank.showHint();
-    this.refreshCloze();
-  }
-
-  onRequestCloseHighlight = (blank: Blank) => {
-    blank.removeTooltip();
-    this.refreshCloze();
-    this.jquery.find("#" + blank.id).focus();
-  }
-
-  onCheckBlank = (blank: Blank) => {
-    this.hideAllHighlights();
-    blank.evaluateEnteredAnswer();
-    this.refreshCloze();
-
-    if (!this.checkAndNotifyCompleteness()) {
-      if (blank.isCorrect) {
-        // move to next blank
-        var index = this.cloze.blanks.indexOf(blank);
-        if (index + 1 >= this.cloze.blanks.length)
-          return;
-        var nextId = this.cloze.blanks[index + 1].id;
-        this.jquery.find("#" + nextId).focus();
-      }
-    }
   }
 
   private hideAllHighlights(): void {
